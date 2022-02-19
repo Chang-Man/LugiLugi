67dae14c6776dc98f0d703351be557480b36a180
Chang-Man <changagim@snu.ac.kr>
2022-02-20 01:06:13 +0900

    Add redux & axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { useMediaQuery } from 'react-responsive';
 
@@ -7,28 +6,16 @@ const Desktop = ({ children }: any) => {
   const isDesktop = useMediaQuery({ minWidth: 992 });
   return isDesktop ? children : null;
 };
-const Tablet = ({ children }: any) => {
+const Mobile = ({ children }: any) => {
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
   return isTablet ? children : null;
-};
-const Mobile = ({ children }: any) => {
-  const isMobile = useMediaQuery({ maxWidth: 767 });
-  return isMobile ? children : null;
-};
-const Default = ({ children }: any) => {
-  const isNotMobile = useMediaQuery({ minWidth: 768 });
-  return isNotMobile ? children : null;
 };
 
 function App() {
   return (
     <div className='App'>
       <Desktop>Desktop</Desktop>
-      <Tablet>
-        <div>아니야</div>
-      </Tablet>
       <Mobile>Mobile</Mobile>
-      {/* <Default>Not mobile</Default> */}
     </div>
   );
 }
